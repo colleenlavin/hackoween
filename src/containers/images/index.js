94ebb66ceac2b23cdc487844baa1bdf924f5e0fb
Colleen Lavin <cml388@mun.ca>
2017-10-29 07:27:59 -0700

    more basics

--- src/containers/images/index.js
+++ src/containers/images/index.js
@@ -1,27 +1,2 @@
-import React, {Component} from 'react'
-import './style.css';
-import Header from '../header'
 
-export default () => (
-    <div>
-          <Header />
 
-          <div className="row"> 
-       
-          <div className="column">
-            <img src="placeholder.jpg" />
-          </div> 
-          <div className="column">
-            <img src="placeholder.jpg" />
-         
-          </div>
-          <div className="column">
-            <img src="placeholder.jpg" />
-            <img src="placeholder.jpg" />
-           
-          </div>
-          
-        </div>
-        
-    </div>
-)
