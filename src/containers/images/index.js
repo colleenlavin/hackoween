ea6ac3687a82ab4b4a967fbabac291152ba4e251
Colleen Lavin <cml388@mun.ca>
2017-10-29 07:30:07 -0700

    Added api outline in images index. Should be able to add stuff from the ge api

--- src/containers/images/index.js
+++ src/containers/images/index.js
@@ -1,2 +1,27 @@
+import React, {Component} from 'react'
+import './style.css';
+import Header from '../header'
 
+export default class Image extends React.Component {
+   constructor() {
+       super();
+       this.state = { items: [] };
+   }
+   
+   componentDidMount() {
+       fetch(``) 
+           .then(result=> {
+               this.setState({items:result.json()});
+           });
+   }
+   
+   render() {        
+       return(
+           <div>
+               <div>Items:</div>
+               { this.state.items.map(item=> { return <div>{}</div>}) }          
+           </div>  
+       );
+   }
+}
 
